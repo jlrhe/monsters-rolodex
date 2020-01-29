481263cc34fbd46e8f3ef33595e71c6ffc9c129b
jlrhe <jan@heikel.fi>
2020-01-30 00:33:18 +0200

    Updates

---
+++ precache-manifest.4f739530e569e427355397190eac71ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223d09af8a8b19cdf8f0cc5c0d680f5b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2cb91d2009ee79b8b2d8",
+    "url": "/monsters-rolodex/static/css/main.f7f3be0b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cb91d2009ee79b8b2d8",
+    "url": "/monsters-rolodex/static/js/main.09f7baba.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
